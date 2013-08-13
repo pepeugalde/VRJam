66eb40afdbd2c60940e2e462747bbb8c24b97e17
pepe <pepe.ugalde@gmail.com>
2013-08-12 20:04:22 -0500

    FIRST

---
+++ Assets/Scripts/DebuggerAngles.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebuggerAngles : MonoBehaviour {
+	
+	public GameObject headCamera;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
