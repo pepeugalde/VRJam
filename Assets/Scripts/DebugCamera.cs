f01cb387a2d2222937c88463c46828ee078096a4
pepe <pepe.ugalde@gmail.com>
2013-08-12 23:35:20 -0500

    Modo debug basico
    tiene un checkbox para deshabilitar oculus y poner camara chafa que se
    mueve con mouse
    si volteas a ver un obj, se imprime en consola
    si dejas de verlo dice cuantos ciclos lo viste

---
+++ Assets/Scripts/DebugCamera.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugCamera : MonoBehaviour {
+	
+	public bool DEBUG_CAMERA;
+	
+	public Camera simpleCamera;
+	public OVRCameraController ovrCamera;
+	
+	void useSimpleCamera () {
+		
+		simpleCamera.enabled = true;
+		ovrCamera.enabled = false;
+		
+		Destroy (ovrCamera.gameObject);
+		ovrCamera = null;
+		
+	}
+	void useOVRCamera () {
+		
+		simpleCamera.enabled = false;
+		ovrCamera.enabled = true;
+		
+		Destroy(simpleCamera.gameObject);
+		simpleCamera = null;
+		
+	}
+	
+	void Awake () {
+		
+	}
+	
+	// Use this for initialization
+	void Start () {
+		if (DEBUG_CAMERA) {
+			//-- usar camara default
+			Debug.Log("USANDO CAMARA SIMPLE");
+			
+			useSimpleCamera();
+			
+		} else {
+			Debug.Log("USANDO OCULUS");
+			
+			useOVRCamera();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (DEBUG_CAMERA) {
+			//-- simpleCamera
+			//Vector3 r = simpleCamera.transform.ToString();
+			//Debug.Log("ROTATION: " + simpleCamera.transform.up);
+				
+			//if (ang.x > 270) {
+			//	Debug.Log ("MIRANDO AL TECHO");
+			//}
+			
+		} else {
+			//-- oculus
+			//Vector3 ang  = Vector3.zero;
+			//bool can = ovrCamera.GetCameraOrientationEulerAngles(ref ang);
+			//Debug.Log("EULER: " + can + " : " + ang);
+			
+		}
+		
+	}
+	
+	
+}
---
+++ Assets/Scripts/StaringScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class StaringScript : MonoBehaviour 
+{
+	public RaycastHit raycastHit;
+	
+	private Collider staredObject;
+	
+	private int stareSec = 0;
+	
+    void Update() 
+    {
+		/*
+        Vector3 CameraCenter = camera.ScreenToWorldPoint(new Vector3(Screen.width/2, Screen.height/2, camera.nearClipPlane));
+        if (Physics.Raycast(CameraCenter, transform.up, 100))
+            Debug.Log("Ou yeah!");
+ 		*/
+		
+		 //Variable which contains information of the gameobject the raycast hits.
+
+		if(Physics.Raycast(transform.position, transform.forward, out raycastHit, 1000.0f)) //Sends out a really long raycast from the camera, and returns information to the variable we created. You can now use it to do something like.
+		{
+			Collider rhc = raycastHit.collider; //We defined a new variable and made it equal to the collider of whatever our raycast hit. We could also make an else statement with a debug.log like so.
+			if (staredObject != rhc) {
+            	staredObject = rhc;
+				Debug.Log("NOW STARING AT: " + staredObject);
+			} else {
+				stareSec++;
+			}
+		} else {
+			if (staredObject != null) {
+				Debug.Log("STOPPED STARING AFTER: " + stareSec);
+				staredObject = null;
+				stareSec = 0;
+			}
+		}
+		
+		
+		
+    }
+}
