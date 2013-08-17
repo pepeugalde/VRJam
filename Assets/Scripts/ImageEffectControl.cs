1169b8544e791a3c0c0e964fd34375defec220df
orionblack <blackorion@gmail.com>
2013-08-16 21:13:53 -0500

    fix

--- Assets/Scripts/ImageEffectControl.cs
+++ Assets/Scripts/ImageEffectControl.cs
@@ -4,40 +4,41 @@ using System.Collections;
 //[AddComponentMenu("Image Effects/Blur/Blur")]
 public class ImageEffectControl : MonoBehaviour {
 
-	public Camera cam;
-	public Camera cam2;
-	BlurEffect bluri;
-	bool blur;
+	public Camera camarablur;
+	public Camera camaranormal;
+	BlurEffect blurefecto;
+	bool bluractivo;
+	
 	void Start () {
-	 bluri= cam.gameObject.GetComponent<BlurEffect>();
-		blur=false;
+	 blurefecto= camarablur.gameObject.GetComponent<BlurEffect>();
+	 bluractivo=false;
 	}
 	
 	
 	void Update () {
 		//chacalazo del blur, cuando esten los assets hago un timer y reviso los parametros a configurar para que se vea chido(y no de un salto)
-		if(blur && bluri.iterations<50){
-			bluri.iterations+=1;
+		if(bluractivo && blurefecto.iterations<50){
+			blurefecto.iterations+=1;
 			
-		} else if (!blur && bluri.iterations>=1){
-			bluri.iterations-=1;
-		}else if(bluri.iterations==0){
+		} else if (!bluractivo && blurefecto.iterations>=1){
+			blurefecto.iterations-=1;
+		}else if(blurefecto.iterations==0 && camarablur.active){
 				
-			cam2.CopyFrom(cam);
-			cam.active=false;
-			cam2.active=true;
+			camaranormal.CopyFrom(camarablur);
+			camarablur.active=false;
+			camaranormal.active=true;
 		}
 		//Copia el contenido de la camara activa en la otra camara inactiva, las intercambia y activa el blur
-		if(Input.GetKeyDown(KeyCode.D) && !blur ){
-			cam.CopyFrom(cam2);
-			cam2.active=false;
-			cam.active=true;
-			blur=true;
+		if(Input.GetKeyDown(KeyCode.D) && !bluractivo ){
+			camarablur.CopyFrom(camaranormal);
+			camaranormal.active=false;
+			camarablur.active=true;
+			bluractivo=true;
 			
-		}else if(Input.GetKeyDown(KeyCode.D) && blur){
+		}else if(Input.GetKeyDown(KeyCode.D) && bluractivo){
 		
 		
-			blur=false;
+			bluractivo=false;
 		}
 		
 	}
